627fe8ddad15a83893eba261c1c1e731ee57d6e2
Erick <al25220365@utcj.edu.mx>
2026-02-24 11:38:03 -0700

    Commit: Trabajando en tetris.js se agrego la logica inicial

--- tetris.js
+++ tetris.js
@@ -1,1 +1,22 @@
 //Archivo para la logica del juego
+class Tetris{
+    constructor(canvasid){
+        this.canvas = document.getElementById(canvasid);
+        this.ctx = this.canvas.getContext('2d');
+        this.gridSize = 20; //Tamaño de cada cuadro
+
+        this.width = 10;
+        this.height = 20;
+        this.board = Array(this.height).fill(0);
+
+        this.score = 0;
+        this.gameOver = false;
+
+        this.pieces = [
+            // I piece
+            [[[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]],[[0,0,1,0],[0,0,1,0],[0,0,1,0],[0,0,1,0]],
+        [[0,0,0,0],[0,0,0,0],[1,1,1,1],[0,0,0,0]],[[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]]],
+        
+        ]
+    }
+}
